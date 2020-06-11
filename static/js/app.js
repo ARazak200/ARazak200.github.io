32a8aea44c86e081b837a24a69f6fd8f5c20b311
ARazak200 <58515893+ARazak200@users.noreply.github.com>
2020-06-11 18:59:10 +0100

    fixed psotion of header and btn

--- static/js/app.js
+++ static/js/app.js
@@ -45,3 +45,17 @@ carouselSlide.addEventListener('transitionend', () => {
 });
 
 
+//fixed header
+window.onscroll = function () { myFunction() };
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
