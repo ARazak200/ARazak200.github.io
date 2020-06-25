aa106e881e7115687982d3017189afb7e44519c0
Ethan <ethanlennox16@gmail.com>
2020-06-25 16:07:05 +0100

    Removed repetitive CSS

--- static/js/autoShow.js
+++ static/js/autoShow.js
@@ -5,7 +5,7 @@ showSlides();
 
 function showSlides() {
 	var i;
-	var slides = document.getElementsByClassName("mySlides");
+	var slides = document.getElementsByClassName("slideshow-container")[0].children;
 	var dots = document.getElementsByClassName("dot");
 	
 	for (i = 0; i < slides.length; i++) {
