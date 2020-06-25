1752158901078f06879daab4de30cb2994126238
Ethan <ethanlennox16@gmail.com>
2020-06-26 00:06:11 +0100

    JavaScript cleanup

--- static/js/autoShow.js
+++ static/js/autoShow.js
@@ -1,8 +1,12 @@
-//AUTO PLAYING SIDESHOW PLAYGROUND
+const slides = Array.from(document.getElementsByClassName("slideshow-container")[0].children);
+const slidesLength = slides.length;
+const dots = Array.from(document.getElementsByClassName("dot"));
+const dotsLength = dots.length;
+const btn = document.getElementsByClassName("slideshow-controller")[0];
 
 var slideIndex = 0;
 var paused = false;  // it starts playing so it is not paused;
-const btn = document.getElementsByClassName("slideshow-controller")[0];
+
 showSlides();
 
 /**
@@ -21,21 +25,18 @@ btn.addEventListener('click', (event) => {
 });
 
 function showSlides() {
-	var i;
-	var slides = document.getElementsByClassName("slideshow-container")[0].children;
-	var dots = document.getElementsByClassName("dot");
-	
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	
+	slides.forEach(slide => {
+		slide.style.display = "none";
+	});
+
+	dots.forEach(dot => {
+		dot.classList.remove("active");
+	});
+
+	slideIndex == slidesLength ? slideIndex = 0 : slideIndex;
+	slides[slideIndex].style.display = "block";
+	dots[slideIndex].classList.add("active");
 	slideIndex++;
-	if (slideIndex > slides.length) { slideIndex = 1 }
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	}
-	slides[slideIndex - 1].style.display = "block";
-	dots[slideIndex - 1].className += " active";
 	
 	if (!paused) {
 		slideshowTimeout = setTimeout(showSlides, 4000); // Change image every 4 seconds
